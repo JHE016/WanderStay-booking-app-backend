23cc81ceb47138bb68332e0a76fc456aae40a324
JinjinHe <he.jinj@northeastern.edu>
2024-10-06 16:51:31 -0700

    Updated cors setup

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     credentials: true,
-    origin: allowedOrigins,
+    origin: 'https://wander-stay-a51091.netlify.app/',
 }));
 app.use(express.static(path.join(__dirname, 'dist')));
 
